ec82e3c96efb3bdefe9525c37dd3772b91524645
Iryna <irasokolova25@ukr.net>
2024-10-14 14:22:35 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello');
+import cartTpl from './cartTpl.hbs'
+import {products} from './cart.js'
+
+const listRef = document.querySelector('.products-list')
+
+
+const cartMarkup = cartTpl(products)
+
+listRef.insertAdjacentHTML('beforeend', cartMarkup)
